32401b102ce01fdc063594a70d505540529d2625
ren <renzhiwei@lewell.cn>
2019-02-25 11:07:45 +0800

    测试添加测试超时时间

--- storage.test.js
+++ storage.test.js
@@ -1,9 +1,25 @@
 import Storage from './storage'
-const storage = new Storage()
- 
 
 test('set value and get value in valid time', () => {
-  storage.set('test', 'test value', 5000)
-  const data = storage.get('test')
+  Storage.set('test', 'test value', 5)
+  const data = Storage.get('test')
   expect(data).toBe('test value')
 });
+
+function sleep(s) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, s * 1000);
+  });
+}
+// 需要设置测试的超时时间, 不然会报错
+// Timeout - Async callback was not invoked within the 5000ms timeout specified by jest.setTimeout.
+// issue: https://github.com/facebook/jest/issues/5055
+test('get value in invalid time', async () => {
+  Storage.clear()
+  Storage.set('test', 'test test value', 2)
+  await sleep(3)
+  const data = Storage.get('test')
+  expect(data).toBeNull()
+}, 5000);
